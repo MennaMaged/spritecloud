c3d98df8cacf634677ba3a4133fb362156762a6a
MennaMaged <menna_maged@yahoo.com>
2021-09-21 11:34:50 +0200

    Format Test class

--- src/test/java/apiTest/LoginTest.java
+++ src/test/java/apiTest/LoginTest.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 import pageObjectModel.login;
 
 public class LoginTest {
-  @Test
-  public void validLogin() {
-	  System.out.println("test");
-	  
-	  login login = new login();
-	  login.performLogin("menna","menna");
-	  
-  }
+	@Test
+	public void validLogin() {
+		System.out.println("test");
+
+		login login = new login();
+		login.performLogin("menna", "menna");
+
+	}
 }
