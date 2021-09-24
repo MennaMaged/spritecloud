d7fc83a397a48e8975eb8854db697081b0188130
MennaMaged <menna_maged@yahoo.com>
2021-09-24 11:03:01 +0200

    Add valid, invalid login TCs

--- src/main/java/pageObjectModel/User.java
+++ src/main/java/pageObjectModel/User.java
@@ -1,40 +1,5 @@
 package pageObjectModel;
 
-import static io.restassured.RestAssured.given;
-
-import org.json.JSONObject;
-
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-
-public class User {
-	
-	private String getLoginServiceName = "v2/user/login";
-
-
-	public void performLogin(String username, String password) {
-		
-		RestAssured.baseURI ="https://petstore.swagger.io/";
-		RequestSpecification request = RestAssured.given();
-
-//		JSONObject requestParams = new JSONObject();
-//		requestParams.put("FirstName", "Virender"); // Cast
-//		requestParams.put("LastName", "Singh");
-//		requestParams.put("UserName", "sdimpleuser2dd2011");
-//		requestParams.put("Password", "password1");	
-//		requestParams.put("Email",  "sample2ee26d9@gmail.com");
-//
-//		request.body(requestParams.toString());
-		Response response = request.get(RestAssured.baseURI+getLoginServiceName+"?username=menna&password=menna");
-
-		int statusCode = response.getStatusCode();
-		System.out.println("The status code recieved: " + statusCode);
-
-		System.out.println("Response body: " + response.body().asString());
-		
-		
-	}
+public class User extends API {
 
 }
--- src/test/java/apiTest/LoginTest.java
+++ src/test/java/apiTest/LoginTest.java
@@ -1,14 +1,27 @@
 package apiTest;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import io.restassured.response.Response;
 import pageObjectModel.User;
 
 public class LoginTest {
+    
+    User user = new User();
+    
     @Test
     public void validLogin() {
-	System.out.println("test");
-	User login = new User();
-	login.performLogin("menna", "menna");
+	System.out.println("First TC - login with valid username and password");
+	Response response = user.login("menna", "menna");
+	Assert.assertEquals(response.getStatusCode(),200);
     }
+    
+    @Test
+    public void invalidLogin() {
+	System.out.println("Second TC - login with invalid username and password");
+	Response response = user.login("", "");
+	Assert.assertEquals(response.getStatusCode(),403);
+    }
+
 }
