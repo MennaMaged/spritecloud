3795213e4d329b85afa213c2c69e8d1d6ba9486e
MennaMaged <menna_maged@yahoo.com>
2021-09-27 00:30:18 +0200

    Add Store TC and its POM

--- src/main/java/pageObjectModel/Store.java
+++ src/main/java/pageObjectModel/Store.java
@@ -1,5 +1,40 @@
 package pageObjectModel;
+import org.json.JSONObject;
 
-public class Store {
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Store extends API{
+
+    // service names
+    private String placeOrderServiceName = "store/order";
+
+    // variables
+    RequestSpecification request = RestAssured.given();
+
+    public Store(String URL) {
+	super(URL);
+    }
+
+    public Response placeOrder(int quantity, String shipDate, String status, Boolean complete) {
+	
+	JSONObject requestParams = new JSONObject();
+	
+	requestParams.put("id", 0); 
+	requestParams.put("petId", 0); 
+	requestParams.put("quantity", 0);
+	requestParams.put("shipDate", shipDate);
+	requestParams.put("status", status);
+	requestParams.put("complete", complete);
+
+	
+	request.header("Content-Type", "application/json");
+	request.body(requestParams);
+	
+	System.out.println(super.baseURL + placeOrderServiceName);
+	return request.post(super.baseURL + placeOrderServiceName);
+
+    }
 
 }
--- src/test/java/apiTest/StoreTest.java
+++ src/test/java/apiTest/StoreTest.java
@@ -1,9 +1,27 @@
 package apiTest;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import io.restassured.response.Response;
+import pageObjectModel.Store;
+
 public class StoreTest {
-  @Test
-  public void f() {
-  }
+    
+private Store store;
+    
+    @Test(priority = 1)
+    public void placeOrder() {
+	System.out.println("Place an order for a pet");
+	Response response = store.placeOrder(1,"2021-09-26T22:23:30.696Z","placed",true);
+	System.out.println(response.asString());
+	Assert.assertEquals(response.getStatusCode(),200);
+    }
+    
+    @BeforeClass
+    public void beforeClass() {
+	System.setProperty("baseURL","https://petstore.swagger.io/v2/");
+	store = new Store(System.getProperty("baseURL"));
+    }
 }
