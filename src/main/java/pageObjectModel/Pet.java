b3f8ea175cefae595e1c2499ced6821b72c7acff
MennaMaged <menna_maged@yahoo.com>
2021-09-27 11:44:41 +0200

    add Pets TCs

--- src/main/java/pageObjectModel/Pet.java
+++ src/main/java/pageObjectModel/Pet.java
@@ -1,5 +1,50 @@
 package pageObjectModel;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class Pet {
+import org.json.JSONObject;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Pet extends API{
+
+    // service names
+    private String addNewPetServiceName = "pet";
+
+    // variables
+    RequestSpecification request = RestAssured.given();
+
+    public Pet(String URL) {
+	super(URL);
+    }
+
+    public Response addNewPet(String category, String name, String [] photoUrls, final String tagName, String status) {
+	Map<String, Object> map = new LinkedHashMap<String, Object>();
+	map.put("tags", Arrays.asList(new LinkedHashMap<String, Object>() {
+	    {
+	        put("id",0);
+	        put("name",tagName);
+	    }
+	}));
+	
+	JSONObject requestParams = new JSONObject();
+	
+	requestParams.put("id", 0); 
+	requestParams.put("category", category); 
+	requestParams.put("name", name);
+	requestParams.put("photoUrls", photoUrls);
+	requestParams.put("tags", map);
+	requestParams.put("status", status);
+	
+	request.header("Content-Type", "application/json");
+	request.body(requestParams);
+	
+	System.out.println(super.baseURL + addNewPetServiceName);
+	return request.post(super.baseURL + addNewPetServiceName);
+
+    }
 
 }
--- src/test/java/apiTest/PetTest.java
+++ src/test/java/apiTest/PetTest.java
@@ -1,9 +1,36 @@
 package apiTest;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import io.restassured.response.Response;
+import pageObjectModel.Pet;
+
 public class PetTest {
-  @Test
-  public void f() {
-  }
+private Pet pet;
+    
+    @Test(priority = 1)
+    public void placeOrder() {
+	String[] PhotoURLs = {"http://google.drive"};
+	System.out.println("Add new pet to the store");
+	Response response = pet.addNewPet("Cats","kitty",PhotoURLs,"testTagName","placed");
+	System.out.println(response.asString());
+	Assert.assertEquals(response.getStatusCode(),200);
+    }
+    
+    @Test(priority = 2, dependsOnMethods = { "placeOrder" })
+    public void placeOrderWithExistingPet() {
+	String[] PhotoURLs = {"http://google.drive"};
+	System.out.println("Add existing pet to the store");
+	Response response = pet.addNewPet("Cats","kitty",PhotoURLs,"testTagName","placed");
+	System.out.println(response.asString());
+	Assert.assertEquals(response.getStatusCode(),405);
+    }
+    
+    @BeforeClass
+    public void beforeClass() {
+	System.setProperty("baseURL","https://petstore.swagger.io/v2/");
+	pet = new Pet(System.getProperty("baseURL"));
+    }
 }
